872bff62e7ddd9f0f37649acb2ce935cb02efdf8
Benjamin Southern <bsouthern@mini.lan>
2022-02-16 13:23:07 -0500

    updated README, install.sh, added nvim comment/indent shortcuts

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # Copies fish config from portable_env git repo into home config.
 mkdir -p ~/.config/fish
-cp ./config.fish ~/.config/fish/
+cp .config/config.fish ~/.config/fish/
 
 # NVIM
-cp -R ./nvim/ ~/.config/
+cp -R .config/nvim/ ~/.config/
 
 # Aliases
-cp ../.aliases ~/
+cp .aliases ~/
