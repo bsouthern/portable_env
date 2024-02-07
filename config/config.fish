a4fe4a934cc4324459c41821f4efc9f2e8b77159
Benjamin Southern <b.southern95@gmail.com>
2024-02-07 18:33:04 -0500

    various updates, but primarily helix editor changes and a few new bash scripts

--- config/config.fish
+++ config/config.fish
@@ -1,13 +1,4 @@
 source ~/.aliases
-# eval "$(/Users/k30505/homebrew/bin/brew shellenv)"
-# set TERM xterm-kitty
-
-# Kitty Shell Integration
-# if set -q KITTY_INSTALLATION_DIR
-#     set --global KITTY_SHELL_INTEGRATION enabled
-#     source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
-#     set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
-# end
  
 # Functions needed for !! and !$
 function __history_previous_command
@@ -39,4 +30,4 @@ else
 end
 
 # Starship Prompt
-starship init fish | source
+# starship init fish | source
